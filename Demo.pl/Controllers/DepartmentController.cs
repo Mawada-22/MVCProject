3f0997a6055632337ce526030da7a6853ea3d1bf
Mawada-22 <mawadaebrahim30@gmail.com>
2025-03-24 07:21:44 +0200

    EditDepartment

--- Demo.pl/Controllers/DepartmentController.cs
+++ Demo.pl/Controllers/DepartmentController.cs
@@ -1,5 +1,6 @@
 ﻿using Demo.BLL.Dtos;
 using Demo.BLL.Services.DepartmentServicea;
+using Demo.pl.Models.Departmets;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Demo.pl.Controllers
@@ -58,6 +59,34 @@ namespace Demo.pl.Controllers
 
             if (department == null) { return NotFound(); };
             return View(department);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if(!id.HasValue)return BadRequest();
+            var department = _services.GetDepartmentById(id.Value);
+
+
+            if (department == null) { return NotFound(); };
+
+            return View(new CreateDepartmentDto() {Code= department.Code,Name=department.Name,Description=department.Description,CreationDate=department.CreationDate });
+
+                                                                   
+        }
+
+        [HttpPost]
+        public IActionResult Edit([FromRoute]int id,DepartmetEditViewModel departmetEditViewModel)
+        {
+            if (!ModelState.IsValid) return View(departmetEditViewModel);
+
+            var department = new UpdateDepartmentDto() { Id=id,Code = departmetEditViewModel.Code, Name = departmetEditViewModel.Name, Description = departmetEditViewModel.Description, CreationDate = departmetEditViewModel.CreationDate };
+           
+            var x =_services.UpdateDepartment(department);
+            if (x > 0) { return RedirectToAction(nameof(Index)); }
+            else { return BadRequest(); }
+
+
         }
     }
 }
---
+++ Demo.pl/Models/Departmets/DepartmetEditViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo.pl.Models.Departmets
+{
+    public class DepartmetEditViewModel
+    {
+
+        [Required(ErrorMessage = "Name is Required")]
+        public string Name { get; set; } = null!;
+        public string Code { get; set; } = null!;
+        public string? Description { get; set; }
+        public DateTime CreationDate { get; set; }
+
+    }
+}
