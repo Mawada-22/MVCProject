e59f5a3bf88ad4d9d1938f6395ca60cfcd2ac0d9
Mawada-22 <mawadaebrahim30@gmail.com>
2025-03-24 09:30:03 +0200

    EmployeeDataFolder

---
+++ Demo.DAL/Entites/Employees/Employee.cs
@@ -0,0 +1,25 @@
+﻿using Demo.DAL.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Entites.Employees
+{
+    public class Employee : ModelBase
+    {
+        public string Name { get; set; } = null!;
+        public int? Age { get; set; }   
+        public decimal Salary { get; set; }
+
+        public bool? IsActive { get; set; } 
+        public string? Address { get; set; }
+        public string? Email { get; set;} 
+        public string? PhoneNumber { get; set;} 
+        public DateOnly HiringDate { get; set; }
+        public Gender gender { get; set; } 
+        public EmpType EmpType { get; set; }
+           
+    }
+}
---
+++ Demo.DAL/Presistance/Data/Migrations/20250324072649_EmployeeModuleMigration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Demo.DAL.Presistance.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmployeeModuleMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: true),
+                    Salary = table.Column<decimal>(type: "decimal(8,2)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: true),
+                    Address = table.Column<string>(type: "varchar(50)", nullable: true, defaultValue: "Cairo"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: true),
+                    HiringDate = table.Column<DateOnly>(type: "date", nullable: false, defaultValueSql: "GETDATE()"),
+                    gender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmpType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedBy = table.Column<int>(type: "int", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastUpdatedBy = table.Column<int>(type: "int", nullable: false),
+                    LastUpdatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
